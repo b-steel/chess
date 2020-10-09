95358fc22337589728f989e85b024bfd0a897b20
Ben Steel <bsteel300@gmail.com>
2020-10-09 07:18:43 -0700

    add tests for Board.py. Passes add_squares test

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
 from mamba import description, context, it, before, after
-from expects import equal, expect, be_a, be
+from expects import equal, expect, be_a, be, have_len
 from places import Square
-from 
+from board import Board
+from pieces import *
 
 with description('Square') as self:
     with before.each:
@@ -36,3 +37,23 @@ with description('Square') as self:
     with it('properties are calculated both ways'):
         self.sq.u = None
         expect(self.sq.ur).to(be(self.sur))
+
+
+with description('Board') as self:
+    with context('create squares'):
+        with before.each:
+            self.b = Board()
+
+        with it('has right size'):
+            expect(self.b.grid).to(have_len(8))
+            expect(self.b.grid[2]).to(have_len(8))
+
+        with it('has a square in each'):
+            for row in range(8):
+                for col in range(8):
+                    expect(self.b.grid[row][col]).to(be_a(Square))
+
+        with it('has squares that know their place correctly'):
+            for row in range(6):
+                for col in range(7):
+                    expect(self.b.grid[row][col].ur).to(be(self.b.grid[row+1][col+1]))
