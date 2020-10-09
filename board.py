5c4529863b6c296a9baa33c11603b9474dfcfee6
Ben Steel <bsteel300@gmail.com>
2020-10-09 11:13:38 -0700

    fix row/col issue with board.py:

--- board.py
+++ board.py
@@ -8,12 +8,12 @@ class Board():
         self.grid = {}
         self.dead_white = Captured()
         self.dead_black = Captured()
+        self.cols = list('abcdefgh')
 
         self.white.opponent = self.black
         self.black.opponent = self.white
         self.create_squares()
         self.create_pieces()
-        self.cols = list('abcdefgh')
 
 
     def create_pieces(self):
@@ -98,16 +98,16 @@ class Board():
         '''Moves the piece from start to end. Assumes valid move
         
         move is (start(col, row), end(col, row))'''
-        start = self.grid[move[0][1]][move[1][0]]
-        end = self.grid[move[1][1]][move[1][0]]
+        start = self.grid[move[0][0]][move[0][1]]
+        end = self.grid[move[1][0]][move[1][1]]
         p = start.piece
         if end.piece:
             self.capture(end.piece)
         end.add_piece(p)
         start.piece = None
         
-    def get_text(self, row, col):
-        return self.grid[row][col].piece.char if self.grid[row][col].piece else '+'
+    def get_text(self, col, row):
+        return self.grid[col][row].piece.char if self.grid[col][row].piece else '+'
 
     def get_dead(self, color):
         if color == 'b':
@@ -122,9 +122,9 @@ class Board():
         s = 'Taken    a b c d e f g h    Taken\n'
         for row in range(7, -1, -1):
             s += f'{{d_b[{(7-row)*2}]}} {{d_b[{(7-row)*2+1}]}}    {row}|'
-            for col in range(8):
-                s += f'{{self.get_text({row},{col})}}'
-                if col != 7:
+            for col in list('abcdefgh'):
+                s += f'{{self.get_text(\'{col}\',{row})}}'
+                if col != 'h':
                     s += ' '
             s +=f'|{row}  {{d_w[{row*2}]}} {{d_w[{row*2+1}]}}\n'
         s += f'         a b c d e f g h'
@@ -132,9 +132,7 @@ class Board():
         d_w = self.get_dead('w')
         d_b = self.get_dead('b')
 
-        text =  f'Taken    a b c d e f g h   Taken\n{d_b[0]} {d_b[1]}    7|{self.get_text(7,0)} {self.get_text(7,1)} {self.get_text(7,2)} {self.get_text(7,3)} {self.get_text(7,4)} {self.get_text(7,5)} {self.get_text(7,6)} {self.get_text(7,7)}|7  {d_w[14]} {d_w[15]}\n{d_b[2]} {d_b[3]}    6|{self.get_text(6,0)} {self.get_text(6,1)} {self.get_text(6,2)} {self.get_text(6,3)} {self.get_text(6,4)} {self.get_text(6,5)} {self.get_text(6,6)} {self.get_text(6,7)}|6  {d_w[12]} {d_w[13]}\n{d_b[4]} {d_b[5]}    5|{self.get_text(5,0)} {self.get_text(5,1)} {self.get_text(5,2)} {self.get_text(5,3)} {self.get_text(5,4)} {self.get_text(5,5)} {self.get_text(5,6)} {self.get_text(5,7)}|5  {d_w[10]} {d_w[11]}\n{d_b[6]} {d_b[7]}    4|{self.get_text(4,0)} {self.get_text(4,1)} {self.get_text(4,2)} {self.get_text(4,3)} {self.get_text(4,4)} {self.get_text(4,5)} {self.get_text(4,6)} {self.get_text(4,7)}|4  {d_w[8]} {d_w[9]}\n{d_b[8]} {d_b[9]}    3|{self.get_text(3,0)} {self.get_text(3,1)} {self.get_text(3,2)} {self.get_text(3,3)} {self.get_text(3,4)} {self.get_text(3,5)} {self.get_text(3,6)} {self.get_text(3,7)}|3  {d_w[6]} {d_w[7]}\n{d_b[10]} {d_b[11]}    2|{self.get_text(2,0)} {self.get_text(2,1)} {self.get_text(2,2)} {self.get_text(2,3)} {self.get_text(2,4)} {self.get_text(2,5)} {self.get_text(2,6)} {self.get_text(2,7)}|2  {d_w[4]} {d_w[5]}\n{d_b[12]} {d_b[13]}    1|{self.get_text(1,0)} {self.get_text(1,1)} {self.get_text(1,2)} {self.get_text(1,3)} {self.get_text(1,4)} {self.get_text(1,5)} {self.get_text(1,6)} {self.get_text(1,7)}|1  {d_w[2]} {d_w[3]}\n{d_b[14]} {d_b[15]}    0|{self.get_text(0,0)} {self.get_text(0,1)} {self.get_text(0,2)} {self.get_text(0,3)} {self.get_text(0,4)} {self.get_text(0,5)} {self.get_text(0,6)} {self.get_text(0,7)}|0  {d_w[0]} {d_w[1]}\n         a b c d e f g h'
-
-
+        text =  f"Taken    a b c d e f g h    Taken\n{d_b[0]} {d_b[1]}    7|{self.get_text('a',7)} {self.get_text('b',7)} {self.get_text('c',7)} {self.get_text('d',7)} {self.get_text('e',7)} {self.get_text('f',7)} {self.get_text('g',7)} {self.get_text('h',7)}|7  {d_w[14]} {d_w[15]}\n{d_b[2]} {d_b[3]}    6|{self.get_text('a',6)} {self.get_text('b',6)} {self.get_text('c',6)} {self.get_text('d',6)} {self.get_text('e',6)} {self.get_text('f',6)} {self.get_text('g',6)} {self.get_text('h',6)}|6  {d_w[12]} {d_w[13]}\n{d_b[4]} {d_b[5]}    5|{self.get_text('a',5)} {self.get_text('b',5)} {self.get_text('c',5)} {self.get_text('d',5)} {self.get_text('e',5)} {self.get_text('f',5)} {self.get_text('g',5)} {self.get_text('h',5)}|5  {d_w[10]} {d_w[11]}\n{d_b[6]} {d_b[7]}    4|{self.get_text('a',4)} {self.get_text('b',4)} {self.get_text('c',4)} {self.get_text('d',4)} {self.get_text('e',4)} {self.get_text('f',4)} {self.get_text('g',4)} {self.get_text('h',4)}|4  {d_w[8]} {d_w[9]}\n{d_b[8]} {d_b[9]}    3|{self.get_text('a',3)} {self.get_text('b',3)} {self.get_text('c',3)} {self.get_text('d',3)} {self.get_text('e',3)} {self.get_text('f',3)} {self.get_text('g',3)} {self.get_text('h',3)}|3  {d_w[6]} {d_w[7]}\n{d_b[10]} {d_b[11]}    2|{self.get_text('a',2)} {self.get_text('b',2)} {self.get_text('c',2)} {self.get_text('d',2)} {self.get_text('e',2)} {self.get_text('f',2)} {self.get_text('g',2)} {self.get_text('h',2)}|2  {d_w[4]} {d_w[5]}\n{d_b[12]} {d_b[13]}    1|{self.get_text('a',1)} {self.get_text('b',1)} {self.get_text('c',1)} {self.get_text('d',1)} {self.get_text('e',1)} {self.get_text('f',1)} {self.get_text('g',1)} {self.get_text('h',1)}|1  {d_w[2]} {d_w[3]}\n{d_b[14]} {d_b[15]}    0|{self.get_text('a',0)} {self.get_text('b',0)} {self.get_text('c',0)} {self.get_text('d',0)} {self.get_text('e',0)} {self.get_text('f',0)} {self.get_text('g',0)} {self.get_text('h',0)}|0  {d_w[0]} {d_w[1]}\n         a b c d e f g h"
 
 
         return text
@@ -144,14 +142,10 @@ class Board():
 
 
 # b = Board()
-# b.move(((1,0), (3,0)))
-# b.capture(b.grid[0][6].piece)
-# b.capture(b.grid[7][5].piece)
-# b.capture(b.grid[7][0].piece)
-# b.capture(b.grid[1][5].piece)
-# b.capture(b.grid[1][3].piece)
-# b.capture(b.grid[1][1].piece)
-# b.capture(b.grid[1][7].piece)
+# b.move((('a',1), ('a',3)))
+# b.capture(b.grid['a'][0].piece)
+# b.capture(b.grid['h'][6].piece)
+
 
 # print(b)
 
