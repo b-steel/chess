c9f9c187eba4307590fc05c9c9c26be119a7111f
Ben Steel <bsteel300@gmail.com>
2020-10-09 12:09:35 -0700

    add team.py

---
+++ team.py
@@ -0,0 +1,11 @@
+from chars import chars
+class Team():
+    def __init__(self, color):
+        self.pieces = []
+        self.opponent = None
+        self.color = color
+
+    def add_piece(self, p):
+        self.pieces.append(p)
+        p.team = self
+        p.char = chars[self.color][p.name]
